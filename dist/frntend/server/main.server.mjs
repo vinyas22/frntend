a54597e1fdb6694ae1f64801958edf771d4e09cb
vinyas22 <vinyasb18@gmail.com>
2025-08-11 17:10:01 +0530

    changed few

--- dist/frntend/server/main.server.mjs
+++ dist/frntend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-Q3U5FA3D.mjs";import"./chunk-PO5NGZQN.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-6FDCUZ3B.mjs";import"./chunk-PO5NGZQN.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  socketUrl: 'http://localhost:5001', // ✅ Correct socket URL
-  apiUrl: 'http://localhost:5001/api'  // ✅ Correct base URL
-// socketUrl: 'wss://billsbackend-7n2f.onrender.com', 
-//   apiUrl: 'https://billsbackend-7n2f.onrender.com/api'
+  // socketUrl: 'http://localhost:5001',
+  // apiUrl: 'http://localhost:5001/api' 
+ socketUrl: 'wss://billsbackend-7n2f.onrender.com', 
+  apiUrl: 'https://billsbackend-7n2f.onrender.com/api'
 };
