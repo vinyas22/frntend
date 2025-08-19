f7830304c61f0f32552a8fa3696eb840958a607a
vinyas22 <vinyasb18@gmail.com>
2025-08-19 22:55:50 +0530

    changed tailwind

--- dist/frntend/server/main.server.mjs
+++ dist/frntend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-AQYJPMXS.mjs";import"./chunk-FYXX56SV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-ULR2NE74.mjs";import"./chunk-Z35H5P3M.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
