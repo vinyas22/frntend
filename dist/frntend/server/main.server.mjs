d41da6b3d801d6665af42ce97f2130af67c387c2
vinyas22 <vinyasb18@gmail.com>
2025-08-19 23:31:07 +0530

    Ready for Render deployment

--- dist/frntend/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-ULR2NE74.mjs";import"./chunk-Z35H5P3M.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
--- dist/frntend/server/render-utils.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{b as o,c as r,d as n}from"./chunk-C6FMPYRA.mjs";import{vb as e}from"./chunk-Z35H5P3M.mjs";import"./chunk-FME56UVT.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
